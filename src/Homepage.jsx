207f75015f05839fda883eccd8571c6246e55667
Rizon1326 <bsse1326@iit.du.ac.bd>
2024-07-01 03:05:02 +0600

    hello2

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,6 +5,7 @@ function Homepage() {
     <div>
       <h1 className='falling-text'>Welcome to Our AI-based solution</h1>
       <p className='falling-subtext'>Project Cool City</p>
+      <button className='get-started-button mt-4'>Get Started</button>
     </div>
   );
 }
